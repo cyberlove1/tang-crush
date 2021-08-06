01964001593ca8e8c4ca2b278bc35a65489645d4
cyberlove1 <88518104+cyberlove1@users.noreply.github.com>
2021-08-06 01:41:42 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Cậu à!',
+    introTitle: 'An à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào tim tớ lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333',
-    mess: 'Anh biết mà. Yêu em nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Có nhiều thể loại phim nma... 😙',
+    desc: 'Bạn xem hentai với toi hong? ',
+    btnYes: 'Có chứ <33',
+    btnNo: 'Không, toi k xem  :3',
+    question:'Trên thế giới nhiều loại phim mà sao cậu chỉ thích hentai <3',
+    btnReply: 'Gửi cho cậu <3',
+    reply: 'Hentai là chân lý',
+    mess: 'Tớ biết mà. Xem chung nhé 😘😘',
+    messDesc: 'Xíu toi gửi link',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://www.facebook.com/profile.php?id=100009012890015'
+    messLink: 'http://www.facebook.com/cancerianjudge'
 }
